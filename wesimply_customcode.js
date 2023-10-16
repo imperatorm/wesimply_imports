42bff7cfa457a595ae51fe4b81db55be70f48b18
imperatorm <32312789+imperatorm@users.noreply.github.com>
2023-10-16 15:57:56 +0200

    add link event listener

--- wesimply_customcode.js
+++ wesimply_customcode.js
@@ -1,7 +1,7 @@
 const links = document.querySelectorAll('a');
 links.forEach((link) => {
     link.addEventListener('click', function (e) {
-        e.eventDefault ();
+        e.preventDefault ();
         let destination = this.href;
 
         console.log(destination)
