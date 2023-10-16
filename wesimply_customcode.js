ed05d3b7b3e46949464e13ab6732d44d0c8c3065
imperatorm <32312789+imperatorm@users.noreply.github.com>
2023-10-16 16:56:30 +0200

    animation items

--- wesimply_customcode.js
+++ wesimply_customcode.js
@@ -1,5 +1,5 @@
 const pageTansition = document.querySelector('.new-page-transition');
-const pageTansitionItems = document.querySelector('.new-page-transition__item');
+const pageTansitionItems = document.querySelectorAll('.new-page-transition__item');
 
 
 const links = document.querySelectorAll('a');
@@ -16,6 +16,25 @@ links.forEach((link) => {
 
         
         pageTansition.style.display = "grid";
-        console.log(destination);
-    });
+        
+        gsap.fromTo(
+            pageTansitionItems,
+            {
+                y: '100vh',
+            
+            },
+            {
+                y: '0vh',
+                duration: 1,
+                ease: "expo.inOut",
+                stagger: {
+                    amount: 0.1,
+                    from: "random",
+
+            },
+            onComplete: () ==> {
+                window.location = destination
+            }
+        }
+    );
 });
