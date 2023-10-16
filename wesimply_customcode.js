4bf08fc9a0878934d4fb58cccd67a0b44abdb2da
imperatorm <32312789+imperatorm@users.noreply.github.com>
2023-10-16 16:14:11 +0200

    location event

--- wesimply_customcode.js
+++ wesimply_customcode.js
@@ -1,6 +1,12 @@
 const links = document.querySelectorAll('a');
 links.forEach((link) => {
     link.addEventListener('click', function (e) {
+        if (
+            this.hostname === window.location.hostname &&
+            this.href.indexOf('#') === -1 &&
+            this.getAttribute("target") !== "_blank"
+        )
+
         e.preventDefault ();
         let destination = this.href;
 
